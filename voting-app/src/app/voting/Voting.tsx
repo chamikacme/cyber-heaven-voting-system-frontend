26ec360e0b1831f7b81370fe404f40223749fe4d
Thenura Kulathunga <thenura@thesanmark.com>
2023-12-14 14:24:39 +0530

    .

---
+++ voting-app/src/app/voting/Voting.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TopNavigation from '../../component/Nvaigation/TopNavigation'
+
+function voting() {
+  return (
+	<section className='w-full flex justify-center items-center'>
+    <TopNavigation/>
+  </section>
+  )
+}
+
+export default voting
---
+++ voting-app/src/component/Nvaigation/TopNavigation.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function TopNavigation() {
+  return (
+    <div className="w-full flex flex-row justify-between">
+      <img
+        className="w-[35px] h-[35px] p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500"
+        src=""
+        alt="Bordered avatar"
+      />
+    </div>
+  );
+}
+
+export default TopNavigation;
