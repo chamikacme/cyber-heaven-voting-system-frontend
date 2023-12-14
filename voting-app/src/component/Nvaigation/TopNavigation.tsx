76a5af0106dcb2c0001640359c1c583d37a9c9aa
Thenura Kulathunga <thenura@thesanmark.com>
2023-12-14 14:47:14 +0530

    .

--- voting-app/src/component/Nvaigation/TopNavigation.tsx
+++ voting-app/src/component/Nvaigation/TopNavigation.tsx
@@ -1,13 +1,11 @@
 import React from "react";
-
+import profileImage from "../../assets/kdh.jpg";
 function TopNavigation() {
   return (
-    <div className="w-full flex flex-row justify-between">
-      <img
-        className="w-[35px] h-[35px] p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500"
-        src=""
-        alt="Bordered avatar"
-      />
+    <div className="w-full flex flex-row justify-between pt-1 px-2">
+      <div className="flex justify-center items-center w-[35px] h-[35px] p-[1px] rounded-full ring-2 ring-cyber-heven-eve-green">
+        <div className="bg-[url('./assets/kdh.jpg')] bg-no-repeat bg-cover bg-center w-[35px] h-[35px] rounded-[35px]"></div>
+      </div>
     </div>
   );
 }
